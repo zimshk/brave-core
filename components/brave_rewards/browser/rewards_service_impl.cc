40aeb862201923412f8c4945c319137a0a3f3568
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-04-24 14:03:46 -0400

    Adds retry on NETWORK_CHANGED error

--- components/brave_rewards/browser/rewards_service_impl.cc
+++ components/brave_rewards/browser/rewards_service_impl.cc
@@ -89,9 +89,9 @@ using net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
-#define RETRIES_COUNT_ON_NETWORK_CHANGE 1
-
 namespace brave_rewards {
+
+const static unsigned int kRetriesCountOnNetworkChange = 1;
 
 class LogStreamImpl : public ledger::LogStream {
  public:
@@ -1180,7 +1180,7 @@ void RewardsServiceImpl::LoadURL(
   net::URLFetcher* fetcher = net::URLFetcher::Create(
       GURL(url), request_type, this).release();
   fetcher->SetRequestContext(g_browser_process->system_request_context());
-  fetcher->SetAutomaticallyRetryOnNetworkChanges(RETRIES_COUNT_ON_NETWORK_CHANGE);
+  fetcher->SetAutomaticallyRetryOnNetworkChanges(kRetriesCountOnNetworkChange);
 
   for (size_t i = 0; i < headers.size(); i++)
     fetcher->AddExtraRequestHeader(headers[i]);
