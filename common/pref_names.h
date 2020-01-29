f98c9c5d5c890d9c59e271c77794b9828b54a906
Guillaume Felley <guillaume@felley.io>
2020-01-29 13:21:46 +0100

    Add toggle button in a dedicated section of the settings to turn-off MRU cycling

--- common/pref_names.h
+++ common/pref_names.h
@@ -77,6 +77,7 @@ extern const char kBinanceRefreshToken[];
 extern const char kBraveGCMChannelStatus[];
 #endif
 extern const char kImportDialogExtensions[];
+extern const char kMRUCyclingEnabled[];
 
 #if defined(OS_ANDROID)
 extern const char kDesktopModeEnabled[];
