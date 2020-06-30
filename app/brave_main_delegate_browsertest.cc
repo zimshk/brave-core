410b0a47d40d41d5eb158bb8fa04be89bcb80b2c
mkarolin <max@brave.com>
2020-06-30 13:33:30 -0400

    Translate bubble changes.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/e11e38149daf37cc5c988047145d236f8a3ba872
    
    commit e11e38149daf37cc5c988047145d236f8a3ba872
    Author: Megan Jablonski <megjablon@chromium.org>
    Date:   Tue May 26 19:41:34 2020 +0000
    
        Remove old Translate Desktop UI and deprecate unused UMA buckets
    
        Bug: 1054643

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -67,7 +67,6 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
       &features::kVideoPlaybackQuality,
 //      &features::kLookalikeUrlNavigationSuggestionsUI,
       &features::kTabHoverCards,
-      &language::kUseButtonTranslateBubbleUi,
   };
 
   for (const auto* feature : disabled_features)
